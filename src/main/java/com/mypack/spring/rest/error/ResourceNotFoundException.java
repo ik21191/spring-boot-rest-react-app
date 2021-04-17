16e0fafb8d729362c68cceffca039e6ee81416ae
Imran Khan <imrankhan.in3@gmail.com>
2021-04-17 14:10:01 +0530

    [v1]:Added log4j2 logging

--- src/main/java/com/mypack/spring/rest/error/ResourceNotFoundException.java
+++ src/main/java/com/mypack/spring/rest/error/ResourceNotFoundException.java
@@ -1,11 +1,14 @@
 package com.mypack.spring.rest.error;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such resource available") 
 public class ResourceNotFoundException extends RuntimeException{
 
+	private static final Logger log = LoggerFactory.getLogger(ResourceNotFoundException.class);
 	/**
 	 * 
 	 */
@@ -13,6 +16,7 @@ public class ResourceNotFoundException extends RuntimeException{
 	
 	public ResourceNotFoundException(String error) {
 		super(error);
+		log.error(error);
 	}
 	
 }
