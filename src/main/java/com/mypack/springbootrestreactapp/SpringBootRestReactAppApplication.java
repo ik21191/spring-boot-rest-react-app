3791f90059bcdd373b4a6fc3968aefcf800f7221
Imran Khan <imrankhan.in3@gmail.com>
2021-04-18 16:03:10 +0530

    [WAR-PACKAGING]: Enabled war packaging

--- src/main/java/com/mypack/springbootrestreactapp/SpringBootRestReactAppApplication.java
+++ src/main/java/com/mypack/springbootrestreactapp/SpringBootRestReactAppApplication.java
@@ -2,12 +2,20 @@ package com.mypack.springbootrestreactapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootRestReactAppApplication {
+public class SpringBootRestReactAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] ar) {
 		SpringApplication.run(SpringBootRestReactAppApplication.class, ar);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		
+		return builder.sources(SpringBootRestReactAppApplication.class);
 	}
 
 }
